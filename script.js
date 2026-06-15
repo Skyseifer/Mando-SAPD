15ebf9513149afe70bb1d19d9140e05d11e70186
Skyseifer <xboxseifer@hotmail.com>
2026-06-15 17:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -339,6 +339,10 @@ function generarSAMS(tipoPaciente){
     const mensaje = `/rff Solicitamos un Alfa en nuestro 10-20 para tratar a ${stringPaciente} en estado ${estado}`;
     mostrarVistaPrevia(mensaje);
     copiarMensaje(mensaje);
+function solicitarCopiaSAMS() {
+    const mensaje = "/rff SAMS me Copia?";
+    mostrarVistaPrevia(mensaje);
+    copiarMensaje(mensaje);
 }
 
 function generarIncautados(){
